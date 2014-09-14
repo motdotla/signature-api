994d91bcb3c059e43164d0d7d25741b4bfed7dd4
scottmotte <scott@scottmotte.com>
2014-09-14 14:25:35 -0700

    headers

--- app.go
+++ app.go
@@ -25,6 +25,7 @@ var (
 func CrossDomain() martini.Handler {
 	return func(res http.ResponseWriter) {
 		res.Header().Add("Access-Control-Allow-Origin", "*")
+		res.Header().Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 	}
 }
 
